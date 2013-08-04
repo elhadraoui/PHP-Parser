a97c71217bd54cbe1ca2fd94fe22b4d128346a65
Hassan Amouhzi <github@anezi.net>
2013-08-04 15:41:49 +0200

    Update Bootstrap.php

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -43,6 +43,8 @@ function __autoload($className)
         $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    
+    echo 'Load: ' . $vCoreLibrary . DIRECTORY_SEPARATOR . $fileName . PHP_EOL;
 
     require $vCoreLibrary . DIRECTORY_SEPARATOR . $fileName;
 }
