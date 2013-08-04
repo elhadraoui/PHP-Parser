8eb381e9df22e08a8d58ca432e78e2e9ddd9c947
Hassan Amouhzi <github@anezi.net>
2013-08-04 12:45:19 +0200

    Update AssignBitwiseAnd.php

--- lib/PHPParser/Node/Expr/AssignBitwiseAnd.php
+++ lib/PHPParser/Node/Expr/AssignBitwiseAnd.php
@@ -1,10 +1,12 @@
 <?php
 
+namespace PHPParser\Node\Expr;
+
 /**
  * @property PHPParser_Node_Expr $var  Variable
  * @property PHPParser_Node_Expr $expr Expression
  */
-class PHPParser_Node_Expr_AssignBitwiseAnd extends PHPParser_Node_Expr
+class AssignBitwiseAnd extends Expr
 {
     /**
      * Constructs an assignment with bitwise and node.
@@ -13,7 +15,7 @@ class PHPParser_Node_Expr_AssignBitwiseAnd extends PHPParser_Node_Expr
      * @param PHPParser_Node_Expr $expr       Expression
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser_Node_Expr $var, PHPParser_Node_Expr $expr, array $attributes = array()) {
+    public function __construct(Expr $var, Expr $expr, array $attributes = array()) {
         parent::__construct(
             array(
                 'var'  => $var,
