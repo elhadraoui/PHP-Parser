6911c508c3efaa21a02cc9e686e5d459cfd9c565
Hassan Amouhzi <github@anezi.net>
2013-08-04 11:26:22 +0200

    Update ErrorTest.php

--- test/PHPParser/Tests/ErrorTest.php
+++ test/PHPParser/Tests/ErrorTest.php
@@ -1,6 +1,8 @@
 <?php
 
-class PHPParser_Tests_ErrorTest extends PHPUnit_Framework_TestCase
+namespace PHPParserTest;
+
+class ErrorTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstruct() {
         $error = new PHPParser_Error('Some error', 10);
