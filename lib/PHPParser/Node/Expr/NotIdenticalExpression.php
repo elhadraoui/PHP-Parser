042fdb6c7088f2b6ba235c389dd6dca1f8b6e566
elhadraoui <naoual.elhadraoui@gmail.com>
2013-08-05 22:14:31 +0200

    Update and rename NotIdentical.php to NotIdenticalExpression.php

--- lib/PHPParser/Node/Expr/NotIdenticalExpression.php
+++ lib/PHPParser/Node/Expr/NotIdenticalExpression.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Expr;
+namespace PHPParser\Node\Expression;
 
 /**
  * @property PHPParser\Node\Expr $left  The left hand side expression
  * @property PHPParser\Node\Expr $right The right hand side expression
  */
-class NotIdentical extends PHPParser\Node\Expr
+class NotIdenticalExpression extends Expression
 {
     /**
      * Constructs a not identical comparison node.
@@ -15,7 +15,7 @@ class NotIdentical extends PHPParser\Node\Expr
      * @param PHPParser\Node\Expr $right      The right hand side expression
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser\Node\Expr $left, PHPParser\Node\Expr $right, array $attributes = array()) {
+    public function __construct(Expression $left, Expression $right, array $attributes = array()) {
         parent::__construct(
             array(
                 'left'  => $left,
