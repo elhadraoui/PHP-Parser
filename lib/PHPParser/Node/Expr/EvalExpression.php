bd7ad19c7d92a05171eaeced7b1df5398b9dc758
elhadraoui <naoual.elhadraoui@gmail.com>
2013-08-05 21:21:12 +0200

    Update and rename Eval.php to EvalExpression.php

--- lib/PHPParser/Node/Expr/EvalExpression.php
+++ lib/PHPParser/Node/Expr/EvalExpression.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace PHPParser\Node;
+namespace PHPParser\Node\Expression;
 
 /**
  * @property PHPParser\Node\Expr $expr Expression
  */
-class Expr_Eval extends PHPParser\Node\Expr
+class EvalExpression extends Expression
 {
     /**
      * Constructs an eval() node.
@@ -13,7 +13,7 @@ class Expr_Eval extends PHPParser\Node\Expr
      * @param PHPParser\Node\Expr $expr       Expression
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser\Node\Expr $expr, array $attributes = array()) {
+    public function __construct(Expression $expr, array $attributes = array()) {
         parent::__construct(
             array(
                 'expr' => $expr
