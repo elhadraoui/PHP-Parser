b82660aee6e377ace027e9a293709d4f436dac8e
elhadraoui <naoual.elhadraoui@gmail.com>
2013-08-05 19:05:40 +0200

    Update Assign.php

--- lib/PHPParser/Node/Expr/Assign.php
+++ lib/PHPParser/Node/Expr/Assign.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Expr;
+namespace PHPParser\Node\Expression;
 
 /**
  * @property PHPParser\Node\Expr $var  Variable
  * @property PHPParser\Node\Expr $expr Expression
  */
-class Assign extends Expr
+class AssignExpression extends Expression
 {
     /**
      * Constructs an assignment node.
@@ -15,7 +15,7 @@ class Assign extends Expr
      * @param PHPParser\Node\Expr $expr       Expression
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser\Node\Expr $var, PHPParser\Node\Expr $expr, array $attributes = array()) {
+    public function __construct(Expression $var, Expression $expr, array $attributes = array()) {
         parent::__construct(
             array(
                 'var'  => $var,
