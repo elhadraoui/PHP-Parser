0d83005fb14c9af20dc7f7ac984b76bbf1ebd73d
elhadraoui <naoual.elhadraoui@gmail.com>
2013-08-05 22:50:28 +0200

    Update and rename SmallerOrEqual.php to SmallerOrEqualExpression.php

--- lib/PHPParser/Node/Expr/SmallerOrEqualExpression.php
+++ lib/PHPParser/Node/Expr/SmallerOrEqualExpression.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Expr;
+namespace PHPParser\Node\Expression;
 
 /**
  * @property PHPParser\Node\Expr $left  The left hand side expression
  * @property PHPParser\Node\Expr $right The right hand side expression
  */
-class SmallerOrEqual extends PHPParser\Node\Expr
+class SmallerOrEqualExpression extends Expression
 {
     /**
      * Constructs a smaller than or equal comparison node.
@@ -15,7 +15,7 @@ class SmallerOrEqual extends PHPParser\Node\Expr
      * @param PHPParser\Node\Expr $right      The right hand side expression
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser\Node\Expr $left, PHPParser\Node\Expr $right, array $attributes = array()) {
+    public function __construct(Expression $left, Expression $right, array $attributes = array()) {
         parent::__construct(
             array(
                 'left'  => $left,
