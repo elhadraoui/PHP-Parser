6231a041fd5fe5a5d0afe548ced5e6de1722c583
elhadraoui <naoual.elhadraoui@gmail.com>
2013-08-05 22:32:51 +0200

    Update and rename PreDec.php to PreDecExpression.php

--- lib/PHPParser/Node/Expr/PreDecExpression.php
+++ lib/PHPParser/Node/Expr/PreDecExpression.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Expr;
+namespace PHPParser\Node\Expression;
 
 /**
  * @property PHPParser\Node\Expr $var Variable
  */
-class PreDec extends PHPParser\Node\Expr
+class PreDecExpression extends Expression
 {
     /**
      * Constructs a pre decrement node.
@@ -13,7 +13,7 @@ class PreDec extends PHPParser\Node\Expr
      * @param PHPParser\Node\Expr $var        Variable
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser\Node\Expr $var, array $attributes = array()) {
+    public function __construct(Expression $var, array $attributes = array()) {
         parent::__construct(
             array(
                 'var' => $var
