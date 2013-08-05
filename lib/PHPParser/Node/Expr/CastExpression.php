3390b331f9f88718c7bf90ba928a9247975134ff
elhadraoui <naoual.elhadraoui@gmail.com>
2013-08-05 20:31:56 +0200

    Update and rename Cast.php to CastExpression.php

--- lib/PHPParser/Node/Expr/CastExpression.php
+++ lib/PHPParser/Node/Expr/CastExpression.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Expr;
+namespace PHPParser\Node\Expression;
 
 /**
  * @property PHPParser\Node\Expr $expr Expression
  */
-abstract class Cast extends PHPParser\Node\Expr
+abstract class CastExpression extends Expression
 {
     /**
      * Constructs a cast node.
@@ -13,7 +13,7 @@ abstract class Cast extends PHPParser\Node\Expr
      * @param PHPParser\Node\Expr $expr       Expression
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser\Node\Expr $expr, array $attributes = array()) {
+    public function __construct(Expression $expr, array $attributes = array()) {
         parent::__construct(
             array(
                 'expr' => $expr
