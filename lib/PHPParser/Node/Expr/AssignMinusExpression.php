5b42d84cdff87d5023f46f6436d222bb83aeb6e5
elhadraoui <naoual.elhadraoui@gmail.com>
2013-08-05 19:30:50 +0200

    Update and rename AssignMinus.php to AssignMinusExpression.php

--- lib/PHPParser/Node/Expr/AssignMinusExpression.php
+++ lib/PHPParser/Node/Expr/AssignMinusExpression.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Expr;
+namespace PHPParser\Node\Expression;
 
 /**
  * @property PHPParser\Node\Expr $var  Variable
  * @property PHPParser\Node\Expr $expr Expression
  */
-class AssignMinus extends PHPParser\Node\Expr
+class AssignMinusExpression extends Expression
 {
     /**
      * Constructs an assignment with minus node.
@@ -15,7 +15,7 @@ class AssignMinus extends PHPParser\Node\Expr
      * @param PHPParser\Node\Expr $expr       Expression
      * @param array               $attributes Additional attributes
      */
-    public function __construct(PHPParser\Node\Expr $var, PHPParser\Node\Expr $expr, array $attributes = array()) {
+    public function __construct(Expression $var, Expression $expr, array $attributes = array()) {
         parent::__construct(
             array(
                 'var'  => $var,
